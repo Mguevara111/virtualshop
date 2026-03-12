fbabedbf4d7b52059b974169413a598094d77f70
el perro <marcelo.guevara1983@gmail.com>
2026-03-12 12:53:49 -0500

    puesto pagina 404

--- src/components/E404.jsx
+++ src/components/E404.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './404.css';
 
 export function E404(){
     return(
-        <section>
-            <h2>error 404</h2>
+        <section className="E404">
+            <h2>Oops! Page Not Found</h2>
+            <Link to='/'>
+                <button className="E404__backbtn">Back to main</button>
+            </Link>
         </section>
     );
 }
